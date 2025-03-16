95f4560f06f4f0ce3b1bdfabe53cfe97e3febaec
kotesh-arya <koteswarraomudila@gmai.com>
2025-03-16 14:12:54 +0530

    REFACTOR: Connected firebase app

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
-import fs from "fs";
-
+import db from "./firebase.js"
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
@@ -22,24 +21,7 @@ app.use((req, res, next) => {
 });
 
 
-// Initialize Firebase Admin SDK
-const serviceAccount = {
-    type: process.env.FIREBASE_TYPE,
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
-};
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-});
-const db = admin.firestore();
+
 
 // Check route
 app.get("/", async (req, res) => {
